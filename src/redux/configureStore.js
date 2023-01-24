da0931781fd6bd045099d26b2d94ff96f0dfe751
Donald Akite <donaldakite27@gmail.com>
2023-01-24 12:24:31 +0000

    Update file name

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import Reducer from './books/books';
+import Reducer from './books/Books';
 import ReducerCategories from './categories/Categories';
 
 export default configureStore({
