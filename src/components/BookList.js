5b65ff9c887d24ffb4ac7165f08592e064ec465e
Donald Akite <donaldakite27@gmail.com>
2023-01-26 09:31:20 +0000

    Connect booklist to redux

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,44 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import Form from './BooksForm';
 
-class BookList extends React.PureComponent {
-  render() {
-    const state = [
-      {
-        key: 0,
-        title: 'Love and thunder',
-        author: 'Donald',
-      },
-      {
-        key: 1,
-        title: 'Evil Guys',
-        author: 'Donald',
-      },
-      {
-        key: 2,
-        title: 'Stirke',
-        author: 'Donald',
-      },
-      {
-        key: 3,
-        title: 'Behold to all',
-        author: 'Donald',
-      },
-    ];
-    return (
-      <>
-        <ul>
-          {state.map((books) => (
-            <li key={books.key}>
-              {' '}
-              <Book title={books.title} author={books.author} />
-            </li>
-          ))}
-        </ul>
-        <Form />
-      </>
-    );
-  }
+function BookList() {
+  const Booklist = useSelector((state) => state.books);
+  return (
+    <>
+      <ul>
+        {Booklist.map((books) => (
+          <li key={books.id}>
+            {' '}
+            <Book book={books} />
+          </li>
+        ))}
+      </ul>
+      <Form />
+    </>
+  );
 }
 export default BookList;
