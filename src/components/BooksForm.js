e1bddd1e148a6c7a552cdbd23e44752395725fd3
Donald Akite <donaldakite27@gmail.com>
2023-01-26 09:31:58 +0000

    Add from functionality

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { AddBook } from '../redux/books/Books';
 
 function Form() {
+  const [userInput, setUserInput] = useState({
+    id: '',
+    genre: '',
+    title: '',
+    author: '',
+  });
+  const dispatch = useDispatch();
+  const handleInput = (e) => {
+    const Name = e.target.name;
+    setUserInput({
+      ...userInput,
+      id: uuidv4(),
+      [Name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AddBook(userInput));
+    setUserInput({
+      title: '',
+      author: '',
+    });
+  };
   return (
     <div>
       <h1>ADD NEW BOOK</h1>
       <div>
-        <form className=" form flex">
+        <form className=" form flex" onSubmit={handleSubmit}>
           <input
             className="book-input"
             placeholder="Boot title"
+            value={userInput.title}
+            name="title"
+            onChange={handleInput}
           />
           <input
             className="Author-input"
             placeholder="Author"
+            name="author"
+            value={userInput.author}
+            onChange={handleInput}
           />
-          <button type="button" className="button">ADD BOOK</button>
+          <button type="submit" className="button">ADD BOOK</button>
         </form>
       </div>
     </div>
